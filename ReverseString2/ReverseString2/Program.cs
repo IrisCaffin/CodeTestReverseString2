d42baf526e0ef6781acf2a1b345e3218f0d1cff1
Iris Caffin <iriscaffin@gmail.com>
2016-05-05 12:29:15 -0700

    Reverse a string using for loop

---
+++ ReverseString2/ReverseString2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str, Revstr = "";
+            Console.Write("Enter a string: ");
+            Str = Console.ReadLine();
+
+            for (int i = Str.Length - 1; i >= 0; i--)
+            {
+                Revstr = Revstr + Str[i];
+            }
+
+            Console.WriteLine("Reverse string is: {0}", Revstr);
+            Console.ReadLine();
+        }
+    }
+}
